7d129a120f7d82787ce9e2a8815e47a5020b8e07
Koji Nakagawa <lightupthesky0627@gmail.com>
2019-10-25 00:09:50 -0700

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t kojinaka/multi-client:latest -t kojinaka/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t kojinaka/multi-server:latest -f kojinaka/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t kojinaka/multi-worker:latest -f kojinaka/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push kojinaka/multi-client:latest
+docker push kojinaka/multi-server:latest
+docker push kojinaka/multi-worker:latest
+
+docker push kojinaka/multi-client:$SHA
+docker push kojinaka/multi-server:$SHA
+docker push kojinaka/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=kojinaka/multi-client:$SHA
+kubectl set image deployments/server-deployment server=kojinaka/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=kojinaka/multi-worker:$SHA
+
